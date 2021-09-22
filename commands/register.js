89e2aeb501d6edd5149a36c87d5c0e0592784132
BuildTools <unconfigured@null.spigotmc.org>
2021-09-22 18:37:20 +0530

    ustilize quickEmbed

--- commands/register.js
+++ commands/register.js
@@ -1,18 +1,19 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+
 const userModel = require('../schema/user');
+const quickEmbed = require('../helpers/quickEmbed');
 
 async function execute(interaction) {
     try {
         await userModel.create({
-            discordID: interaction.user.id,
+            discordId: interaction.user.id,
             regGames: [],
             regPlatforms: [],
             level: 0,
         });
-        await interaction.reply("Successfully registered!");
+        quickEmbed.success(interaction, "Successfully registered. Next, connect your steam / (valo / etc) account");
     } catch (err) {
-        console.error(err);
-        await interaction.reply("Error during registration");
+        quickEmbed.error(interaction, "Error during registration");
     }
 }
 
