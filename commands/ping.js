e21231650a36d520655e6443e962a55364bc84dd
BuildTools <unconfigured@null.spigotmc.org>
2021-09-22 12:14:57 +0530

    Expanded the simple example command

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,19 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageActionRow, MessageButton } = require('discord.js');
 
 async function execute(interaction) {
-    return interaction.reply('Pong!');
+    const row = new MessageActionRow()
+        .addComponents(
+            new MessageButton()
+                .setCustomId('ping.primary')
+                .setLabel('Again')
+                .setStyle('PRIMARY'),
+        );
+    await interaction.reply({ content: 'Pong!', components: [row] });
+}
+
+async function handleButton(interaction) {
+    await interaction.update({ content: 'Don\'t ping me again!', components: [] });
 }
 
 module.exports = {
@@ -9,4 +21,5 @@ module.exports = {
         .setName('ping')
         .setDescription('Replies with Pong!'),
     execute,
+    handleButton,
 };
