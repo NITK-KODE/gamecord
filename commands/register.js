3f7d8685fbee65815341d4439cb5c2ecdc39813a
BuildTools <unconfigured@null.spigotmc.org>
2021-09-22 13:55:38 +0530

    Register a fresh user

--- commands/register.js
+++ commands/register.js
@@ -1,7 +1,19 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const userModel = require('../schema/user');
 
 async function execute(interaction) {
-    await interaction.reply("Command not implemented !");
+    try {
+        await userModel.create({
+            discordID: interaction.user.id,
+            regGames: [],
+            regPlatforms: [],
+            level: 0,
+        });
+        await interaction.reply("Successfully registered!");
+    } catch (err) {
+        console.error(err);
+        await interaction.reply("Error during registration");
+    }
 }
 
 module.exports = {
